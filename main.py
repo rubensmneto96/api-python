e895561f9658fe8e63ed169d2249edf0793ea448
Rubens <rubensmn96@gmail.com>
2025-12-03 18:38:28 -0300

    Corrigido comentario do metodo 'convert_dict()', criado regras para adicionar usuarios e criado routes para completar o CRUD.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Users(db.Model):
 	username : Mapped[str] = mapped_column(nullable=False)
 	user_admin : Mapped[bool] = mapped_column(nullable=False)
 
-	# Necessario converter os dados para JSON
+	# Converte os dados para um dicionario para depois converte-los em JSON com o metodo .jsonify()
 
 	def convert_dict(self):
 		return {
@@ -42,8 +42,8 @@ def hello():
 @app.route("/users", methods=["GET"])
 def list_users():
 	users = Users.query.all() # Executa como se fosse um SELECT na tabela
-	
-	return jsonify([users.convert_dict()] for u in users)
+
+	return jsonify([u.convert_dict() for u in users]) # Retorna os dados convertidos em JSON
 
 
 # Cria usuarios novos
@@ -51,15 +51,56 @@ def list_users():
 def user_add():
 	data = request.get_json()
 
+	users = Users.query.all()
+	usr = [u.convert_dict() for u in users]
+
 	novo_user = Users(
 			username=data["username"],
 			user_admin=data["user_admin"]
 		)
 
-	db.session.add(novo_user)
-	db.session.commit()
+	usernames = [i["username"] for i in usr]
+
+	if novo_user.username in usernames:
+		return jsonify({"error": "Usuario ja existe."}), 404
+	elif novo_user.user_admin == "":
+		return jsonify({"error": "'username' nao pode ser nulo."}), 404
+	elif novo_user.username == "":
+		return jsonify({"error": "'user_admin' nao pode ser nulo."}), 404
+	else:
+		db.session.add(novo_user)
+		db.session.commit()
 
 	return jsonify(novo_user.convert_dict()), 201
+	
+
+# Modifica usuarios
+@app.route("/users/<int:id_user>", methods=["PUT"])
+def update_user(id_user):
+	data = request.get_json()
+
+	user = Users.query.get(id_user)
+	if user:
+		user.username = data.get("username", user.username)
+		user.user_admin = data.get("user_admin", user.user_admin)
+
+		db.session.commit()
+
+		return jsonify(user.convert_dict())
+	else:
+		return jsonify({"error": "Usuario nao existe"}), 404
+
+# Deleta usuarios
+@app.route("/users/<int:id_user>", methods=["DELETE"])
+def delete_user(id_user):
+	user = Users.query.get(id_user)
+	if user:
+		db.session.delete(user)
+		db.session.commit()
+
+		return jsonify({"message": "Usuario deletado."})
+	else:
+		return jsonify({"error": "Usuario nao existe."}), 404
 
 
 
